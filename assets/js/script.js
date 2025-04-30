1067cdb712efe546c8c6c5c4014a6dee487d359c
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-04-30 19:15:34 +0100

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,35 @@
 /** Games Area **/
 
     /** Controls Area **/
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorectAnswers(){
+
+}
+
 
     /** Questions Area **/
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+}
+
+function displayMultiplyQuestion(){
+}
 
     /** Score Area **/
+function incrementScore(){
+
+}
+    
+function incrementWongAnswetr(){
+    
+}
