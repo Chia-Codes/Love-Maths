5e32a9f67348804d64d2479a51f2ad6f2ed715bf
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-05-03 16:27:48 +0100

    Tidy up favicon and set answer box to empty after submitting

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,6 +24,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+    // Sets the answer box to empty string
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
