c57955d7d482ca5bccd7134e938b65ae236e16be
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-05-03 14:22:57 +0100

    Display addition questions to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,71 @@
-// Listen for DOM content to be loaded then executes function
-// Code to be executed when the user clicks a button
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-    button.addEventListener("click", function() {
-        if (this.getAttribute("data-type") === "submit") {
-            alert("You clicked Submit!");
-        } else {
-            let gameType = this.getAttribute("data-type");
-            alert(`You clicked ${gameType}`);
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
             }
         });
     }
-})
 
-/** Games Area **/
+    runGame("addition");
 
-    /** Controls Area **/
-    /**
+});
+
+/**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame(){
-    // Creates 2 random number between 1 & 25
-    // Math.random() returns a number between 0 and 1, so we multiply by 25 to get a number between 0 and 25
- let num1 = Math.floor(Math.random() * 25) + 1;
- let num2 = Math.floor(Math.random() * 25) + 1;
-}
+function runGame(gameType) {
 
-function checkAnswer(){
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
-}
-
-function calculateCorectAnswers(){
-
-}
-
-
-    /** Questions Area **/
-function displayAdditionQuestion(){
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
 
-function displaySubtractQuestion(){
-}
-
-function displayMultiplyQuestion(){
-}
-
-    /** Score Area **/
-function incrementScore(){
+function checkAnswer() {
 
 }
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
     
-function incrementWongAnswetr(){
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
     
 }
